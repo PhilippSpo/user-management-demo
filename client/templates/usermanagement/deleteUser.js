c6bd6415cbabaf806d75c3d3849f0cc2ed5db671
PhilippSpo <philipp.sporrer@planifica.de>
2015-02-23 23:58:57 +0100

    cleanup fixing for safari/chrome updated some polymer stuff

--- client/templates/usermanagement/deleteUser.js
+++ client/templates/usermanagement/deleteUser.js
@@ -1,5 +1,5 @@
 Template.deleteUser.events({
-	'tap #deleteUser': function() {
+	'tap #deleteUser': function(e) {
 		Meteor.call('removeUser', Router.current().params.userId, function(error, result) {
 			if (!error) {
 				// show message
@@ -9,3 +9,15 @@ Template.deleteUser.events({
 		});
 	}
 });
+
+// Template.deleteUser.rendered = function () {
+// 	this.find('#deleteUser').addEventListener('click', function(e) {
+// 		Meteor.call('removeUser', Router.current().params.userId, function(error, result) {
+// 			if (!error) {
+// 				// show message
+// 				Router.go('umShowUsers');
+// 				GlobalUI.toast("Nutzer wurde entfernt");
+// 			}
+// 		});
+// 	});
+// };
