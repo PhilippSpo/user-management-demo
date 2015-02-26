5321b09ee2dd4f227e0847c6ba3f0fc1925c3e7a
PhilippSpo <philipp.sporrer@planifica.de>
2015-02-26 17:31:32 +0100

    added language switch

--- client/templates/layout/header.js
+++ client/templates/layout/header.js
@@ -1,16 +1,32 @@
 Template.publicHeaderItems.events({
-	'click [data-action=sign-in]': function () {
+	'click [data-action=sign-in]': function() {
 		Router.go('atSignIn');
-	},'click [data-action=sign-out]': function (e) {
+	},
+	'click [data-action=sign-out]': function(e) {
 		e.preventDefault();
 		Meteor.logout(function() {
 			Router.go('atSignIn');
 		});
 	},
-	'click [data-action=showUsers]': function () {
+	'click [data-action=showUsers]': function() {
 		Router.go('umShowUsers');
 	},
-	'click [data-action=showGroups]': function () {
+	'click [data-action=showGroups]': function() {
 		Router.go('umShowGroups');
+	}
+});
+
+Template.language.events({
+	'click [data-action=english]': function() {
+		TAPi18n.setLanguage('en')
+			.done(function() {
+				UserManagementTemplates.setupSchemaMessages();
+			});
+	},
+	'click [data-action=german]': function() {
+		TAPi18n.setLanguage('de')
+			.done(function() {
+				UserManagementTemplates.setupSchemaMessages();
+			});
 	}
 });
