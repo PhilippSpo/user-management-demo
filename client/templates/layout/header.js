3314c9b3f3a48fee1de29580de5c5e4549b3d7f0
Lukas Sporrer <lukas.sporrer@softbricks.de>
2015-02-26 14:31:05 +0100

    switch language

--- client/templates/layout/header.js
+++ client/templates/layout/header.js
@@ -1,9 +1,38 @@
 Template.publicHeaderItems.events({
 	'click [data-action=sign-in]': function () {
 		Router.go('atSignIn');
-	},'click [data-action=sign-out]': function () {
+	},
+	'click [data-action=sign-out]': function () {
 		Meteor.logout(function() {
 			Router.go('atSignIn');
 		});
+	}
+});
+
+Template.language.events({
+	'click [data-action=englisch]': function () {
+		TAPi18n.setLanguage('en')
+			.done(function() {
+				UserManagementTemplates.setupSchemaMessages();
+			});
+	},
+	'click [data-action=german]': function () {
+		TAPi18n.setLanguage('de')
+			.done(function() {
+				UserManagementTemplates.setupSchemaMessages();
+			});
+	},
+	'click': function(e){
+		if(e.target.id == "de"){
+			TAPi18n.setLanguage('de')
+				.done(function() {
+					UserManagementTemplates.setupSchemaMessages();
+				});
+		}else if(e.target.id == "en"){
+			TAPi18n.setLanguage('en')
+				.done(function() {
+					UserManagementTemplates.setupSchemaMessages();
+				});
+		}
 	}
 });
